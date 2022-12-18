b7bb2568f0186fa52d92510fc5bd457d93be2041
Bjarni123 <Bjarni04@gmail.com>
2022-12-18 21:29:45 +0000

    dah 1

---
+++ JS_files/main.js
@@ -0,0 +1,11 @@
+import WorkoutProgram from "./JSON_files/workoutProgram.json";
+
+jsonOutput = document.getElementById("jsonOutput");
+
+/*
+confirmButton.addEventListener("click", function() {
+    var output = textInput.value;
+    jsonOutput.innerHTML = output;
+});
+*/
+console.log(WorkoutProgram);
